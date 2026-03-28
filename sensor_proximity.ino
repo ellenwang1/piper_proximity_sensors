a5411422ae6b5da51e32ca18a4caf98f4d6a3ed0
Ellen Wang <ellen.huiwang@gmail.com>
2026-03-28 17:09:46 +1100

    proximity sensor arduino sketch

---
+++ sensor_proximity.ino
@@ -0,0 +1,65 @@
+#include <micro_ros_arduino.h>
+#include <stdio.h>
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <std_msgs/msg/int32.h>
+
+rcl_publisher_t publisher;
+std_msgs__msg__Int32 msg;
+rclc_executor_t executor;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+rcl_timer_t timer;
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; (void)temp_rc;}
+
+void error_loop(){
+  while(1){
+    delay(100);
+  }
+}
+
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time) 
+{  
+  RCLC_UNUSED(last_call_time);
+  if (timer != NULL) {
+    long sum = 0;
+    for(int i = 0; i < 10; i++){
+      sum += touchRead(T6);
+      delay(2);
+    }
+    msg.data = (int32_t)(sum / 10);
+    RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
+  }
+}
+
+void setup() {
+  set_microros_transports();
+  delay(2000);
+  allocator = rcl_get_default_allocator();
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+  RCCHECK(rclc_node_init_default(&node, "micro_ros_arduino_node", "", &support));
+  RCCHECK(rclc_publisher_init_default(
+    &publisher,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    "proximity_raw"));
+  const unsigned int timer_timeout = 1000;
+  RCCHECK(rclc_timer_init_default(
+    &timer,
+    &support,
+    RCL_MS_TO_NS(timer_timeout),
+    timer_callback));
+  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+  RCCHECK(rclc_executor_add_timer(&executor, &timer));
+  msg.data = 0;
+}
+
+void loop() {
+  delay(100);
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+}
