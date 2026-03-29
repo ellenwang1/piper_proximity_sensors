6bd8a24eb8e1af61979cfa980eb38e88df3e18b2
Ellen <ellen.huiwang@gmail.com>
2026-03-29 14:57:25 +1100

    Use two pins for training first

--- sensor_proximity.ino
+++ sensor_proximity.ino
@@ -2,15 +2,19 @@
 #include <std_msgs/Int32.h>
 
 ros::NodeHandle nh;
-std_msgs::Int32 msg;
-ros::Publisher pub("proximity_raw", &msg);
+
+std_msgs::Int32 msg_t6, msg_t9;
+
+ros::Publisher pub_t6("proximity/t6", &msg_t6);
+ros::Publisher pub_t9("proximity/t9", &msg_t9);
 
 unsigned long last_publish = 0;
 const unsigned long PUBLISH_INTERVAL = 1000; // ms
 
 void setup() {
   nh.initNode();
-  nh.advertise(pub);
+  nh.advertise(pub_t6);
+  nh.advertise(pub_t9);
 }
 
 void loop() {
@@ -19,13 +23,11 @@ void loop() {
   if (now - last_publish >= PUBLISH_INTERVAL) {
     last_publish = now;
 
-    long sum = 0;
-    for (int i = 0; i < 10; i++) {
-      sum += touchRead(T6);
-      delay(2);
-    }
-    msg.data = (int32_t)(sum / 10);
-    pub.publish(&msg);
+    msg_t6.data = (int32_t)touchRead(T6);
+    msg_t9.data = (int32_t)touchRead(T9);
+
+    pub_t6.publish(&msg_t6);
+    pub_t9.publish(&msg_t9);
   }
 
   nh.spinOnce();
